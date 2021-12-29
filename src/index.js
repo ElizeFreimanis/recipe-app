ba5229dc24ed8d1aa2cd4a706891223406f02fc1
Elize Freimanis <elizesf@hotmail.com>
2021-12-29 15:26:18 +0100

    modified route

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from './App';
 ReactDOM.render(
     <BrowserRouter>
         <Routes>
-            <Route path='/' element={<App />}>
+            <Route path='/recipe-app/' element={<App />}>
                 <Route index element={<Home />} />
                 <Route path=':id' element={<FullRecipe />} />
             </Route>
